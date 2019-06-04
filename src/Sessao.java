660668748e02400d0aba93d3663e2ff7900279d4
srjveiga <estgv15849@alunos.estgv.ipv.pt>
2019-06-04 10:56:16 +0100

    class sessão - alterado o construtor para receber: ano, mês, dia, hora e minuto - LocalDate.of e LocalTime.of

--- src/Sessao.java
+++ src/Sessao.java
@@ -9,9 +9,9 @@ public class Sessao {
     Sala sala;
     Livro livro;
 
-    public Sessao(LocalDate data, LocalTime hora, ClubeLivro clubelivro, Sala sala, Livro livro) {
-        this.data = data;
-        this.hora = hora;
+    public Sessao(int year, int month, int day, int hour, int minute, ClubeLivro clubelivro, Sala sala, Livro livro) {
+        this.data = LocalDate.of(year, month, day);
+        this.hora = LocalTime.of(hour, minute);
         this.clubelivro = clubelivro;
         this.sala = sala;
         this.livro = livro;
