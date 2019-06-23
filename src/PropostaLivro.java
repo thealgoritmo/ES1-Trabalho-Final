45529207fd3b5ed12519ecbd7e943479f8c793ba
António <antonio_castela@hotmail.com>
2019-06-23 12:07:56 +0100

    Alteracao propostalivro

--- src/PropostaLivro.java
+++ src/PropostaLivro.java
@@ -6,27 +6,21 @@ public class PropostaLivro {
     Membro membro;
     ClubeLivro clubeLivro;
 
-    public LocalDate getData() {
-        return data;
+    public PropostaLivro(LocalDate data, Membro membro, ClubeLivro clubeLivro) {
+        this.data = data;
+        this.membro = membro;
+        this.clubeLivro = clubeLivro;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
+    public LocalDate getData() {
+        return data;
     }
 
     public Membro getMembro() {
         return membro;
     }
 
-    public void setMembro(Membro membro) {
-        this.membro = membro;
-    }
-
     public ClubeLivro getClubeLivro() {
         return clubeLivro;
-    }
-
-    public void setClubeLivro(ClubeLivro clubeLivro) {
-        this.clubeLivro = clubeLivro;
     }
 }
