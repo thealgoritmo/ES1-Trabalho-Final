b3161ee65fbc3009938a9635d26617479ea4194d
António <antonio_castela@hotmail.com>
2019-06-25 18:51:05 +0100

    Main errado

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,36 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String args[]){
-        Utilizador u = new Utilizador("Manuel","Ativo", new TipoUtilizador("aluno",10));
-        PropostaAquisicao propostaAquisicao = new PropostaAquisicao(LocalDate.now(),u,"Alice no Pais das Maravilhas");
-        Livro l = new Livro("Programar em Java", "FCA");
-        Copia c = new Copia(1, l);
-        RequisicaoCompraCopia requisicaoCompraCopia = new RequisicaoCompraCopia("pendente", LocalDate.now(), "Oficio",l,u);
-        Encomenda encomenda = new Encomenda(LocalDate.now(),requisicaoCompraCopia);
-        EntradaNovoLivro entradaNovoLivro = new EntradaNovoLivro(LocalDate.now(),encomenda,c);
+       /* TipoUtilizador t = new TipoUtilizador("aluno",10);
+        Utilizador u = new Utilizador("Manuel","Ativo", t);
+        ClubeLivro c = new ClubeLivro(1,"António","Ficção-Cientifica","Ativo");
+        LocalDate d = LocalDate.now();
+        Membro m = new Membro(u,c,d);
         RepositorioMem repo = new RepositorioMem();
-        repo.adicionaEntradaNovoLivro(entradaNovoLivro);
+        repo.adicionaMembro(m);*/
+
+        /*TipoUtilizador t = new TipoUtilizador("aluno",10);
+        Utilizador u = new Utilizador("Manuel","Ativo", t);
+        ClubeLivro c = new ClubeLivro(1,"António","Ficção-Cientifica","Ativo");
+        Livro l = new Livro("Viagem","Porto Editora", "Ficção-Cientifica");
+        LocalDate d = LocalDate.now();
+        Membro m = new Membro(u,c,d);
+        d = LocalDate.now();
+        PropostaLivro p = new PropostaLivro(d,m,c);
+        d = LocalDate.now();
+        AprovacaoPropostaLivro ap = new AprovacaoPropostaLivro("Em Espera",d,m,c,p);
+        RepositorioMem repo = new RepositorioMem();
+        repo.adicionaAprovacaoPropostaLivro(ap);*/
+
+        TipoUtilizador t = new TipoUtilizador("aluno",10);
+        Utilizador u = new Utilizador("Manuel","Ativo", t);
+        ClubeLivro c = new ClubeLivro(1,"António","Ficção-Cientifica","Ativo");
+        Livro l = new Livro("Viagem","Porto Editora", "Ficção-Cientifica");
+        Sala s = new Sala(1,"Sala Grande");
+        Sessao ss = new Sessao(2019,06,10,13,30,c,s,l);
+        RepositorioMem repo = new RepositorioMem();
+        repo.adicionaSessao(ss);
+
     }
 
 }
